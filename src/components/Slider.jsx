d6bad6118110709507c210a6074cadc10c76a017
Burak Karaman <karaman.buraak@gmail.com>
2024-03-17 19:53:22 +0100

    Create Slider.jsx

---
+++ src/components/Slider.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo1 from '../assets/shopping1.jpg'
+import logo2 from '../assets/shopping2.jpg'
+import logo3 from '../assets/shopping3.jpg'
+import { Carousel } from "react-bootstrap";
+
+const Slider = () => {
+  return (
+      <div className='carousel-div'> 
+       <Carousel variant='dark'  className='carousel'  autoPlay={true} interval={4000}>
+          <Carousel.Item>
+              <img className="" src={logo1} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+              <img className="" src={logo2} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+              <img className="" src={logo3} alt="" />
+          </Carousel.Item>
+      </Carousel></div>
+  )
+}
+
+export default Slider
