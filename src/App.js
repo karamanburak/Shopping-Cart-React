cf356b750912a0ce4b6f8f2d851ff97a237e1d34
burak <karaman.buraak@gmail.com>
2024-03-17 11:29:20 +0100

    Create folder structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from './router/AppRouter';
+
+
+
+function App() {
+
+
+
+  return (
+    <div className="app">
+      <AppRouter />
+
+    </div>
+  );
+}
+
+export default App;
