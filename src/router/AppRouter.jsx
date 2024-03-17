d02cbcea93930c82b199950fc2820fa712e86c07
burak <karaman.buraak@gmail.com>
2024-03-17 11:33:50 +0100

    Create useState Hooks & data fetched

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../pages/Main'
 import NewProduct from '../pages/NewProduct'
 import ProductList from '../pages/ProductList'
 import UpdateProduct from '../pages/UpdateProduct'
 import About from '../pages/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import spinner from '../assets/loading.gif'
+
 
 const AppRouter = () => {
+    const [newProduct, setNewProduct] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //! GET (READ)\\
+    const BASE_URL = "https://65f4c515f54db27bc0224f20.mockapi.io/products"
+
+
+    const getData = async () => {
+
+        const res = await axios.get(BASE_URL)
+        // console.log(res.data);
+
+        setNewProduct(res.data)
+        setLoading(false)
+
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
+    if (loading) {
+        return (
+            <div className=' text-center mt-5'>
+                <img src={spinner} alt="" width="100px" />
+            </div>
+        )
+
+    }
   return (
       <div>
           <Router>
