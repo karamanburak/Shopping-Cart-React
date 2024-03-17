7dc9bce9d80116463230a340eaa32bc347de7fe4
burak <karaman.buraak@gmail.com>
2024-03-17 11:32:07 +0100

    Update AppRouter from home to anathor pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Main from '../pages/Main'
+import NewProduct from '../pages/NewProduct'
+import ProductList from '../pages/ProductList'
+import UpdateProduct from '../pages/UpdateProduct'
+import About from '../pages/About'
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+      <div>
+          <Router>
+              <MyNavbar />
+              <Routes>
+                  <Route exact path='/' element={<Main />} />
+                  <Route path='/newproduct' element={<NewProduct  />} />
+                  <Route path='/productlist' element={<ProductList  />} />
+                  <Route path='/updateproduct' element={<UpdateProduct  />} />
+                  <Route path='/about' element={<About />} />
+              </Routes>
+          </Router>
+
+      </div>
   )
 }
 
