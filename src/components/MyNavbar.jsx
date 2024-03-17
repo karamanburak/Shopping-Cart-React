2ca97dc6d68c5d5e8bb2f61fb6d337f51b0a3d93
burak <karaman.buraak@gmail.com>
2024-03-17 11:35:39 +0100

    Update MyNavbar.jsx

---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const MyNavbar = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Navbar className='navbar' expand="lg" >
+      <Container className='navbar-container'>
+
+        <div className=' navbar-div'>
+
+          <h3 onClick={() => navigate("/")} className='text-white' style={{ cursor: "pointer" }}>Techno Markt</h3>
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" >
+              <Nav className="me-auto">
+                <NavLink
+                  className="nav-link"
+                  to="/"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  className="nav-link"
+                  to="/newproduct"
+                >
+                  New Product
+                </NavLink>
+                <NavLink
+
+                  className="nav-link"
+                  to="/productlist"
+                >
+                  Product List
+                </NavLink>
+                <NavLink
+
+                  className="nav-link"
+                  to="/about"
+                >
+                  About
+                </NavLink>
+              </Nav>
+              <Link to="/productlist" >
+                <img className='cart-logo' src={logo} alt="" width="100px" height="80px" />
+              </Link>
+            </Navbar.Collapse>
+          </div>
+
+        </div>
+
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar
