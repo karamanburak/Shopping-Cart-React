234cc4ce1d2dd76691a4472bc8c3ce9e52ba7003
burak <karaman.buraak@gmail.com>
2024-03-17 11:36:41 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import avatar from '../assets/avatar.png'
+import { FaGithub } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="container mt-5">
-      <div className="row d-flex justify-content-center">
+    <div className="container mt-5" >
+      <div className="row d-flex justify-content-center" >
         <div className="col-md-7">
           <div className="card p-3 py-4">
             <div className="text-center">
               <img
-                src="https://avatars.githubusercontent.com/u/93030628?v=4"
+                src={avatar}
                 width={100}
                 className="rounded-circle"
                 alt="a"
@@ -18,10 +20,10 @@ const About = () => {
               <span className="bg-secondary p-1 px-4 rounded text-white">
                 Pro
               </span>
-              <h5 className="mt-2 mb-0">Anthony Harold</h5>
-              <span>Fullstack Developer</span>
+              <h5 className="mt-2 mb-0 fs-3 text-danger" style={{ textDecoration: "underline" }}>Burak Karaman</h5>
+              <span className="fs-5">Fullstack Developer</span>
               <div className="px-4 mt-1">
-                <p className="fonts">
+                <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
                   voluptatem dolore sint quo assumenda eum voluptate aut, sequi
                   rem cumque facilis asperiores possimus porro, facere eveniet!
@@ -31,36 +33,34 @@ const About = () => {
               </div>
               <ul className="social-list">
                 <li>
-                  <i className="fa-brands fa-facebook" />
+                  <i className="fa-brands fa-dribbble" />
                 </li>
                 <li>
-                  <i className="fa-brands fa-dribbble" />
+                  <i className="fa-brands fa-facebook" />
                 </li>
                 <li>
                   <i className="fa-brands fa-instagram" />
                 </li>
                 <li>
-                  <i className="fa-brands fa-linkedin" />
-                </li>
-                <li>
-                  <i className="fa-brands fa-google" />
+                  <i className="fa-brands fa-x" />
                 </li>
               </ul>
               <div className="buttons">
                 <button className="btn btn-outline-primary px-4">
                   <a
                     className="text-decoration-none text-black "
-                    href="https://github.com/anthonyharold67"
+                    href="https://wwww.karaman.buraak@gmail.com"
                     target="true">
+                    <i className="gmail fa-brands fa-google " />
                     Message
                   </a>
                 </button>
                 <button className="btn btn-primary px-4 ms-3">
                   <a
                     className="text-decoration-none text-white"
-                    href="https://github.com/anthonyharold67"
+                    href="https://github.com/karamanburak"
                     target="true">
-                    Contact
+                    <FaGithub className="github" />  Contact
                   </a>
                 </button>
               </div>
