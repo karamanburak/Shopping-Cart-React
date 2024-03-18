b95f993bbe9f1d3040aa50b343bce024410c5e0f
Burak Karaman <karaman.buraak@gmail.com>
2024-03-18 23:32:22 +0100

    Add new buttons  to navigate to other pages (NotFound.jsx)

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import notFound from '../assets/notFound.jpeg'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+    const navigate = useNavigate()
     return (
-        <div className="message-div ">
-             <img className='rounded-4' src={notFound} alt="" width="500px"/>
-        </div>
+       
+            <div className="message-div ">
+            <div className='mx-5'>
+
+            <img className='rounded-4' src={notFound} alt="" width="500px" /> 
+            </div>
+                <div className='mt-4 text-center'>
+                <button onClick={()=>navigate("/")} className='btn btn-outline-danger mx-2 my-4 fw-bold'>GO HOME PAGE</button>
+                <button onClick={() => navigate("/newproduct")} className='btn btn-outline-primary mx-2 fw-bold'>GO ADD PRODUCT PAGE</button>
+                <button onClick={() => navigate("/productlist")} className='btn btn-outline-warning mx-2 fw-bold'>GO PRODUCT LIST PAGE</button>
+                </div>
+            </div>
+           
+             
+           
+
+      
     )
 }
 
