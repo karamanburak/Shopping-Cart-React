5e9c255e2034bc1e4d2874678eed15882216b390
Burak Karaman <karaman.buraak@gmail.com>
2024-03-18 23:37:15 +0100

    Update NotFound.jsx (feat: buttons style)

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,9 +12,9 @@ const NotFound = () => {
             <img className='rounded-4' src={notFound} alt="" width="500px" /> 
             </div>
                 <div className='mt-4 text-center'>
-                <button onClick={()=>navigate("/")} className='btn btn-outline-danger mx-2 my-4 fw-bold'>GO HOME PAGE</button>
-                <button onClick={() => navigate("/newproduct")} className='btn btn-outline-primary mx-2 fw-bold'>GO ADD PRODUCT PAGE</button>
-                <button onClick={() => navigate("/productlist")} className='btn btn-outline-warning mx-2 fw-bold'>GO PRODUCT LIST PAGE</button>
+                <button onClick={()=>navigate("/")} className='btn btn-danger mx-2 my-4 fw-bold'>GO HOME PAGE</button>
+                <button onClick={() => navigate("/newproduct")} className='btn btn-primary mx-2 fw-bold'>GO ADD PRODUCT PAGE</button>
+                <button onClick={() => navigate("/productlist")} className='btn btn-warning mx-2 fw-bold'>GO PRODUCT LIST PAGE</button>
                 </div>
             </div>
            
